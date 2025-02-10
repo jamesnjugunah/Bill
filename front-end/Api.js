c1b392911dd65853e33a66c8c0d690283254638c
james_njugunah <145548430+jamesnjugunah@users.noreply.github.com>
2025-02-10 17:11:45 +0300

    api

--- front-end/Api.js
+++ front-end/Api.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 const API_BASE_URL = 'http://localhost:5000/api';
 
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+      "Content-Type": "application/json",
+    },
+});
+
 
 const registerUser = async (email, phone, password, confirmPassword) => {
 
@@ -10,5 +17,16 @@ const registerUser = async (email, phone, password, confirmPassword) => {
         window.location.href = response.data.redirect;
     } catch (error) {
         alert(error.response?.data?.error || "Registration failed");
+    }
+}
+
+//handling login page
+const loginUser = async (email, password) => {
+    try {
+        const response = await axios.post(`${API_BASE_URL}/login`, { email, password });
+        alert(response.data.message);
+        window.location.href = response.data.redirect;
+    } catch (error) {
+        alert(error.response?.data?.error || "Login failed");
     }
 }
